6283c2f457ac2c3eefbff3a001cc43482180a1d8
PressJump <40802367+PressJump@users.noreply.github.com>
2022-10-06 12:26:36 +1100

    🎉 Extension Base

---
+++ src/background.js
@@ -0,0 +1,29 @@
+const qr_generator = browser.menus.create({
+    title: 'qr-generator',
+    contexts: ["all"],
+    documentUrlPatterns: ['*://*/*'],
+})
+
+browser.menus.onClicked.addListener(function (info, tab) {
+    switch (info.menuItemId) {
+        case qr_generator:
+            //get the url of the current tab
+            browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                var url = tabs[0].url;
+                //create window with index.html 500x500 in the center of the screen, no border
+                browser.windows.create({
+                    url: "index.html",
+                    type: "popup",
+                    width: 500,
+                    height: 500,
+                    left: Math.round((screen.availWidth - 500) / 2),
+                    top: Math.round((screen.availHeight - 500) / 2),
+                    focused: true,
+                    state: "normal"
+                });
+                
+            });
+
+        break
+    }
+})
