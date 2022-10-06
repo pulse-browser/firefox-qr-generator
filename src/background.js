2f04eee22256a3432cb4e806cf956f116ad64b7e
PressJump <40802367+PressJump@users.noreply.github.com>
2022-10-06 12:35:17 +1100

    ✨ Send QR through Parameter

--- src/background.js
+++ src/background.js
@@ -12,15 +12,16 @@ browser.menus.onClicked.addListener(function (info, tab) {
                 var url = tabs[0].url;
                 //create window with index.html 500x500 in the center of the screen, no border
                 browser.windows.create({
-                    url: "index.html",
+                    url: "index.html?url=" + url,
                     type: "popup",
                     width: 500,
                     height: 500,
-                    left: Math.round((screen.availWidth - 500) / 2),
-                    top: Math.round((screen.availHeight - 500) / 2),
+                    left: Math.round((screen.width/2)-(500/2)),
+                    top: Math.round((screen.height/2)-(500/2)),
                     focused: true,
-                    state: "normal"
+                    state: "normal",
                 });
+                
                 
             });
 
