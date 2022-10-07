a42d151e1057e2e5f8d660cfe12ab036b8f963f7
PressJump <40802367+PressJump@users.noreply.github.com>
2022-10-08 09:16:59 +1100

    Ui Changes (Sizing&BrowserStyle)

--- src/background.js
+++ src/background.js
@@ -1,30 +1,28 @@
 const qr_generator = browser.menus.create({
-    title: 'qr-generator',
+    title: "qr-generator",
     contexts: ["all"],
-    documentUrlPatterns: ['*://*/*'],
-})
+    documentUrlPatterns: ["*://*/*"],
+});
 
 browser.menus.onClicked.addListener(function (info, tab) {
     switch (info.menuItemId) {
         case qr_generator:
             //get the url of the current tab
-            browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
                 var url = tabs[0].url;
                 //create window with index.html 500x500 in the center of the screen, no border
                 browser.windows.create({
                     url: "index.html?url=" + url,
                     type: "popup",
-                    width: 500,
-                    height: 500,
-                    left: Math.round((screen.width/2)-(500/2)),
-                    top: Math.round((screen.height/2)-(500/2)),
+                    width: 220,
+                    height: 300,
+                    left: Math.round(screen.width / 2 - 500 / 2),
+                    top: Math.round(screen.height / 2 - 500 / 2),
                     focused: true,
                     state: "normal",
                 });
-                
-                
             });
 
-        break
+            break;
     }
-})
+});
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ var url = new URL(window.location.href);
 var url = url.searchParams.get("url");
 var qrcode = new QRCode(document.getElementById("qrcode"), {
     text: url,
-    width: 128,
-    height: 128,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
+    width: 205,
+    height: 205,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H,
 });
