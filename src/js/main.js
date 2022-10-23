13f2aeb00ee3fcac191c78bebeb5857277e2f632
PressJump <40802367+PressJump@users.noreply.github.com>
2022-10-23 20:46:08 +1100

    🩹 QR Qualification

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,8 @@ async function generateQRCode() {
 
     var qrcode = new QRCode(document.getElementById("qrcode"), {
         text: url,
-        width: 205,
-        height: 205,
+        width: 150,
+        height: 150,
         colorDark: "#000000",
         colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H,
